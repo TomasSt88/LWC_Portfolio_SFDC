3435754377b452c7f2a7ac2620d6b8d40ca999ff
Tomas Stranak <tomas.stranak@accenture.com>
2024-02-08 14:16:40 +0100

    Skills component and static resouces added

--- force-app/main/default/lwc/skillCards/skillCards.js
+++ force-app/main/default/lwc/skillCards/skillCards.js
@@ -2,7 +2,10 @@ import { LightningElement } from 'lwc';
 import IMAGES from '@salesforce/resourceUrl/Images'
 
 export default class SkillCards extends LightningElement {
-    adminImageURL = IMAGES + '/admin.png';
-    trainingImageURL = IMAGES + '/training.png';
-    serviceImageURL = IMAGES + '/service.png';
+    configImageURL = IMAGES + '/config.png';
+    backendImageURL = IMAGES + '/backend.png';
+    frontendImageURL = IMAGES + '/frontend.png';
+    dataImageURL = IMAGES + '/data.png';
+    softwaregImageURL = IMAGES + '/software.png';
+    softskillsImageURL = IMAGES + '/softskills.png';
 }
