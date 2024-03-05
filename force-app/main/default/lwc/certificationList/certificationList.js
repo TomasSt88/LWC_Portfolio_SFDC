32a3a27fde28d0235b3a0fc3715578290ba2fbd0
Tomas Stranak <tomas.stranak@accenture.com>
2024-03-05 13:13:50 +0100

    FrontEnd certification UI review

--- force-app/main/default/lwc/certificationList/certificationList.js
+++ force-app/main/default/lwc/certificationList/certificationList.js
@@ -1,11 +1,10 @@
 import { LightningElement, api, wire } from 'lwc';
 import { subscribe, MessageContext } from 'lightning/messageService';
 import FILTERSCHANGEMC from '@salesforce/messageChannel/FiltersChange__c';
-import getCertificates from '@salesforce/apex/aboutMeCustomPortfolioController.getCertificates';
+import getCertificatesAndContentVersionIds from '@salesforce/apex/aboutMeCustomPortfolioController.getCertificatesAndContentVersionIds';
 
 export default class CertificationList extends LightningElement {
     @api recordId;
-    certificatesData = { portfolio: null };
     certifications = [];
 
     @wire(MessageContext)
@@ -26,17 +25,13 @@ export default class CertificationList extends LightningElement {
     }
 
     handleMessage(message) {
-        console.log('Received message:', message);
-        
         if (message.type === 'reset') {
-            this.certificatesData = { portfolio: null };
+            this.certifications = [];
         } else if (message.portfolioId) {
             let portfolioIdStr = message.portfolioId.toString();
-            console.log('Fetching portfolio with ID:', portfolioIdStr);
-            
-            getCertificates({ portfolioId: portfolioIdStr })
+            getCertificatesAndContentVersionIds({ portfolioId: portfolioIdStr })
             .then(certificatesResult => {
-                this.certifications = certificatesResult; // Changed this line
+                this.certifications = certificatesResult;
                 console.log('Fetched certificates:', this.certifications);
             })
             .catch(error => {
