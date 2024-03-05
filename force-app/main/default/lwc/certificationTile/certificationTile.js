a452f7fd9ded97ca837fe2e60ec4ea980783d70f
Tomas Stranak <tomas.stranak@accenture.com>
2024-03-05 13:13:50 +0100

    CSS Certificates Updates

--- force-app/main/default/lwc/certificationTile/certificationTile.js
+++ force-app/main/default/lwc/certificationTile/certificationTile.js
@@ -1,21 +1,8 @@
 import { LightningElement, api } from 'lwc';
-import getContentVersionIds from '@salesforce/apex/aboutMeCustomPortfolioController.getContentVersionIds';
 
 export default class CertificationTile extends LightningElement {
     @api certification;
-    imageUrl;
-
-    connectedCallback() {
-        this.fetchImageUrl();
-    }
-
-    fetchImageUrl() {
-        getContentVersionIds({ portfolioId: this.certification.Id })
-        .then(contentDocumentIdResult => {
-            this.imageUrl = '/sfc/servlet.shepherd/version/download/' + contentDocumentIdResult;
-        })
-        .catch(error => {
-            console.error('Error fetching ContentDocumentId:', error);
-        });
+    get imageUrl() {
+        return '/sfc/servlet.shepherd/version/download/' + this.certification['ContentVersion Id'];
     }
 }
